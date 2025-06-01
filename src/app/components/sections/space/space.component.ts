e25f0af5177b2208cab45bdcd521825121b06642
GabrielCarvalho17 <gabrielcarvalho17@outlook.com>
2025-06-01 13:57:23 -0300

    Domingo

--- src/app/components/sections/space/space.component.ts
+++ src/app/components/sections/space/space.component.ts
@@ -1,35 +1,72 @@
 import { Component } from '@angular/core';
-import { NgClass, NgForOf, NgStyle } from '@angular/common';
-import { ServiceComponent } from '../../cards/service/service.component';
 
 @Component({
   selector: 'app-space',
-  imports: [NgForOf, NgClass, NgStyle],
+  imports: [],
   templateUrl: './space.component.html',
   styleUrl: './space.component.scss',
 })
 export class SpaceComponent {
-  features = [
+  elements = [
     {
-      icon: 'fa-seedling',
-      title: 'Ambiente Acolhedor',
-      text: 'Um espaço planejado para transmitir tranquilidade, conforto e conexão com a natureza desde a chegada.',
+      class: 'space__element-image',
+      type: 'image',
+      src: 'caminho-da-imagem.jpg',
+      alt: 'img1',
     },
     {
-      icon: 'fa-user-md',
-      title: 'Estrutura Profissional',
-      text: 'Ambientes bem equipados para garantir atendimentos terapêuticos com privacidade, segurança e eficiência.',
+      class: 'space__element-info',
+      type: 'info',
+      title: 'Título do Bloco',
+      desc:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry, ' +
+        'Lorem Ipsum has been, the industry standard dummy.',
+      link: 'Mais sobre',
     },
     {
-      icon: 'fa-child',
-      title: 'Espaço Infantil',
-      text: 'Espaços adaptados para o atendimento de crianças, com brinquedos terapêuticos e atmosfera lúdica.',
+      class: 'space__element-image',
+      type: 'image',
+      src: '...',
+      alt: 'img2',
     },
-  ];
-  images = [
-    { src: 'assets/imagem1.jpg', alt: 'Descrição da imagem 1' },
-    { src: 'assets/imagem2.jpg', alt: 'Descrição da imagem 2' },
-    { src: 'assets/imagem3.jpg', alt: 'Descrição da imagem 3' },
-    { src: 'assets/imagem4.jpg', alt: 'Descrição da imagem 4' },
+    {
+      class: 'space__element-info',
+      type: 'info',
+      title: 'Título do Bloco',
+      desc:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry, ' +
+        'Lorem Ipsum has been, the industry standard dummy.',
+      link: 'Mais sobre',
+    },
+    {
+      class: 'space__element-info',
+      type: 'info',
+      title: 'Título do Bloco',
+      desc:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry, ' +
+        'Lorem Ipsum has been, the industry standard dummy.',
+      link: 'Mais sobre',
+    },
+    {
+      class: 'space__element-image',
+      type: 'image',
+      src: '/images/spaces/space-05.jpg',
+      alt: 'img3',
+    },
+    {
+      class: 'space__element-info',
+      type: 'info',
+      title: 'Título do Bloco',
+      desc:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry, ' +
+        'Lorem Ipsum has been, the industry standard dummy.',
+      link: 'Mais sobre',
+    },
+    {
+      class: 'space__element-image',
+      type: 'image',
+      src: '...',
+      alt: 'img4',
+    },
   ];
 }
