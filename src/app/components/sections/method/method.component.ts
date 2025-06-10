07fb4b28a6c1be6ef557879584e0ba955e6970b7
gcarvalho-labs <gabrielcarvalho17@outlook.com>
2025-06-10 13:25:37 -0300

    Finalização do Stepper

--- src/app/components/sections/method/method.component.ts
+++ src/app/components/sections/method/method.component.ts
@@ -13,41 +13,44 @@ export class MethodComponent {
       title: 'Converse conosco',
       description:
         'Entre em contato para entender suas necessidades e esclarecer dúvidas.',
-      image: {src: '/images/chat_with_us.svg', alt: 'img1'},
+      image: { src: '/images/chat_with_us.svg', alt: 'img1' },
       vClass: 'stepper__step--left',
     },
     {
       title: 'Agende um horário',
       description:
         'Escolha o melhor dia e horário para iniciar seu atendimento.',
-      image: {src: '/images/schedule_appointment.svg', alt: 'img1'},
+      image: { src: '/images/schedule_appointment.svg', alt: 'img2' },
       vClass: 'stepper__step--right',
     },
     {
       title: 'Escolha o seu profissional',
       description:
         'Selecione o especialista que melhor se adapta ao seu perfil.',
-      image: {src: '/images/choice_professional.svg', alt: 'img1'},
+      image: { src: '/images/choice_professional.svg', alt: 'img3' },
       vClass: 'stepper__step--left',
     },
     {
       title: 'Inicie o seu processo',
       description:
         'Comece sua jornada com um plano personalizado de acompanhamento.',
-      image: {src: '...', alt: 'img1'},
+      image: { src: '/images/start_process.svg', alt: 'img4' },
       vClass: 'stepper__step--right',
     },
     {
-      title: 'Acompanhe a sua evolução',
-      description: 'Monitore os avanços e ajustes ao longo do processo.',
-      image: {src: '...', alt: 'img1'},
+      title: 'Evolua no seu ritmo',
+      description:
+        'Com o apoio do seu psicólogo, você irá perceber conquistas e ' +
+        'avanços no seu tempo, construindo bem-estar e autoconhecimento.',
+      image: { src: '/images/evolve_calmly.svg', alt: 'img5' },
       vClass: 'stepper__step--left',
     },
     {
-      title: 'Conclua com alta',
+      title: 'Sinta-se preparado para seguir',
       description:
-        'Finalize o processo com segurança e orientação para o futuro.',
-      image: {src: '...', alt: 'img1'},
+        'Ao final do processo, você se sentirá mais fortalecido para ' +
+        'encarar os desafios da vida com autonomia e confiança.',
+      image: { src: '/images/feel_ready.svg', alt: 'img6' },
       vClass: 'stepper__step--right',
     },
   ];
