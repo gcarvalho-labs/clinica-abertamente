00d0aa29da33d23773e7d253a7ee67a25881998d
GabrielCarvalho17 <gabrielcarvalho17@outlook.com>
2025-06-27 20:48:04 -0300

    Footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { NgClass, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgForOf, NgClass],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  menus = [
+    { label: 'Serviço', href: '#' },
+    { label: 'Equipe', href: '#' },
+    { label: 'Metodologia', href: '#' },
+    { label: 'Espaço', href: '#' },
+    { label: 'Blog', href: '#' },
+    { label: 'Contato', href: '#' },
+  ];
+
+  networks = [
+    { href: '#', iconClass: 'fab fa-facebook-f' },
+    { href: '#', iconClass: 'fab fa-linkedin-in' },
+    { href: '#', iconClass: 'fab fa-x-twitter' },
+    { href: '#', iconClass: 'fab fa-instagram' },
+  ];
+}
